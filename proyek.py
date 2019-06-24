ad7324fa1dd01886b9cada3a0093e5ec5a2f0499
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2019-06-24 17:27:43 +0700

    Update proyek.py

--- proyek.py
+++ proyek.py
@@ -57,14 +57,14 @@ def banner():
      \rw+==============================+
            \rwThanks to \raIvana Raa/
            \rwCreated by \raIqbal Dev
-          \rd==========================''')
+        \rd==========================''')
 
     
 
 def login():
     banner()
-    user_name = raw_input(p+"  [log]" + a + " Username" + pu + ": ")
-    password = raw_input(p+"  [log]" + a + " Password" + pu + ": ")
+    user_name = raw_input(p+"  [log]" + a + " Username" + k + ": ")
+    password = raw_input(p+"  [log]" + a + " Password" + k + ": ")
     req = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+user_name+'&locale=en_US&password='+password+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
     
     dev = req.content
@@ -190,6 +190,8 @@ def pilih():
         lagi = raw_input(h+" [?]" +pu+ " Cracking Lagi?" +a+ " [y/n]: " +k+ "")
         if lagi == "y" or lagi == "yes" or lagi == "lagi":
             mbf()
+        elif lagi == "n" or lagi == "no" or lagi == "tidak":
+            sys.exit()
         else:
             print 
             print m+" Pilih Yg bener Cuk..."
